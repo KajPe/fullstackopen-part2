24db7fc1efd844aeeea2bba80b2a7eb39e6b2741
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-07-23 23:02:35 +0300

    Teht 2.6

---
+++ Puhelinluettelo/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Person from  './components/Person'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        { name: 'Arto Hellas' }
+      ],
+      newName: ''
+    }
+  }
+
+  handleNewnameChange = (event) => {
+    this.setState({ 
+      newName: event.target.value
+    })
+  }
+
+  addNewname = (event) => {
+    event.preventDefault()
+  
+    const personObject = {
+      name: this.state.newName
+    }
+
+    const persons = this.state.persons.concat(personObject)
+
+    this.setState({
+      persons: persons,
+      newName: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <form onSubmit={this.addNewname}>
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleNewnameChange} />
+          </div>
+          <div>
+            <button type="submit">lisää</button>
+          </div>
+        </form>
+        <h2>Numerot</h2>
+        <div>
+          {this.state.persons.map(person => <Person key={person.name} person={person} />)}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
