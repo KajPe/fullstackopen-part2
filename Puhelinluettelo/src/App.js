7c3a1c20674c20848053168bc95a4f1030046f35
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-07-23 23:05:04 +0300

    Teht 2.11

--- Puhelinluettelo/src/App.js
+++ Puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 import Search from  './components/Search'
 import Persons from  './components/Persons'
 
@@ -6,16 +7,19 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ],
+      persons: [],
       newName: '',
       newNumber: '',
       search: ''
     }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        this.setState({ persons: response.data })
+      })
   }
 
   setSearch = (s) => {
